944dc752c230411089632b1c2cd722265e045bc4
Venkatesh Kannan Karuppaiah <venkateshkannankaruppaiah@Venkateshs-MacBook-Pro.local>
2020-06-25 06:39:26 -0700

    binarySearch Tree Implementation

---
+++ BinarySearchTreeImplementation/BinarySearchTreeImplementation/BinarySearchTree.cs
@@ -0,0 +1,128 @@
+﻿using System;
+namespace BinarySearchTreeImplementation
+{
+    public class BinarySearchTree
+    {
+        Node root;
+
+        public  BinarySearchTree()
+        {
+            this.root = null;   
+        }
+
+        public class Node
+        {
+            public Node right;
+            public Node left;
+            public int data;
+        }
+
+
+        public void insert(int d)
+        {
+            Node currentNode;
+            Node parent;
+            Node newNode = new Node(); 
+            newNode.data = d;
+
+            if (root == null)
+            {
+                root = newNode;
+                return;
+            }
+
+            currentNode = root;
+            parent = root;
+
+            do
+            {
+                if (currentNode.data < d)
+                {
+                    parent = currentNode;
+                    currentNode = currentNode.right;
+                    if (currentNode == null)
+                        parent.right = newNode;
+                    
+                }
+                else
+                {
+                    parent = currentNode;
+                    currentNode = currentNode.left;
+                    if (currentNode == null)
+                        parent.left = newNode;
+                }
+
+            } while (currentNode != null);
+        }
+
+
+        public void display()
+        {
+            Console.WriteLine("InOrder");//sorted format
+            inOrder(root);
+            Console.WriteLine("PreOrder");
+             preOrder(root);
+            Console.WriteLine("PostOrder");
+            postOrder(root);
+
+        }
+
+        public void inOrder(Node root)
+        {
+            if (root == null)
+                return;
+
+            inOrder(root.left);
+            Console.Write(" " + root.data +" ");
+            inOrder(root.right);
+        }
+
+        public void preOrder(Node root)
+        {
+            if (root == null)
+                return;
+
+            Console.Write(" " + root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+
+        public void postOrder(Node root)
+        {
+            if (root == null)
+                return;
+            
+            postOrder(root.left);
+            postOrder(root.right);
+            Console.Write(" " + root.data + " ");
+        }
+
+        public bool search(int d)
+        {
+            Node currentNode = root;
+
+            if (root.data == d)
+            {
+                return true;
+            }
+
+            do
+            {               
+                if (currentNode.data < d)
+                {
+                    currentNode = currentNode.right;
+                    if (currentNode!=null && currentNode.data == d)
+                    { return true; }
+                }
+                else
+                {
+                    currentNode = currentNode.left;
+                    if (currentNode!=null &&  currentNode.data == d)
+                    { return true; }
+                }
+            } while (currentNode != null);
+        
+            return false;
+        }
+    }
+}
---
+++ BinarySearchTreeImplementation/BinarySearchTreeImplementation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BinarySearchTreeImplementation
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("BST !");
+            BinarySearchTree bst = new BinarySearchTree();
+            bst.insert(8);
+            bst.insert(6);
+            bst.insert(14);
+            bst.insert(9);
+            bst.insert(7);
+            bst.insert(5);
+            Console.WriteLine("BST values ! ");
+            bst.display();
+
+            Console.WriteLine("bst.search(6) " + bst.search(6).ToString());
+            Console.WriteLine("bst.search(8) " + bst.search(8).ToString());
+            Console.WriteLine("bst.search(14) " + bst.search(14).ToString());
+            Console.WriteLine("bst.search(9) " + bst.search(9).ToString());
+            Console.WriteLine("bst.search(7) " + bst.search(7).ToString());
+            Console.WriteLine("bst.search(5) " + bst.search(5).ToString());
+            Console.WriteLine("bst.search(30) " + bst.search(3).ToString());
+            Console.WriteLine("bst.search(21) " + bst.search(21).ToString());
+            Console.WriteLine("bst.search(3) " + bst.search(3).ToString());
+            Console.ReadLine();
+        }
+    }
+
+
+}
